8a8a803b53b09206c63dec76c55217c7a6327404
cribdragg3r <joshua.faust@sevrosecurity.com>
2022-11-05 10:56:29 -0500

    dataupdates

--- download_data.py
+++ download_data.py
@@ -1,10 +1,10 @@
-from PyForks.trailforks_region import TrailforksRegion
-from PyForks.trailforks_user import TrailforksUser
+from PyForks.region import Region
+from PyForks.user import User
 import pandas as pd
 from tqdm import tqdm
 
 USERNAME = "mnmtb"
-PASSWORD = ""
+PASSWORD = "8AjsoS8R!szAX8#4%SrwF*"
 
 region_to_id_map = {
     "battle-creek-5538": "5538", 
@@ -12,10 +12,13 @@ region_to_id_map = {
     "lebanon-hills": "3438",
     "murphyhanrehan-park": "7646",
     "theodore-wirth-park": "9224",
-    "xcel-energy-mountain-bike-park-50624": "50624"
+    "xcel-energy-mountain-bike-park-50624": "50624",
+    "carver-lake-park": "9243",
+    "monarch-carver-park-reserve": "41480",
+    "minnesota-river-bottoms-25053": "25053"
 }
 
-tf_region = TrailforksRegion(username=USERNAME, password=PASSWORD)
+tf_region = Region(username=USERNAME, password=PASSWORD)
 tf_region.login()
 
 trails_files_dfs = []
@@ -31,5 +34,5 @@ for region, region_id in region_to_id_map.items():
 pbar.close()
 
 pd.concat(trails_files_dfs).to_csv("data/region_trails.csv", index=False)
-pd.concat(trail_ridecounts_dfs).to_csv("data/region_ridecounts.csv")
-pd.concat(trail_ridelogs_dfs).to_csv("data/region_ridelogs.csv")
+pd.concat(trail_ridecounts_dfs, ignore_index=True).to_csv("data/region_ridecounts.csv")
+pd.concat(trail_ridelogs_dfs, ignore_index=True).to_csv("data/region_ridelogs.csv")
